e90c1bc0131a44ae744dafbe7b1194b148e1fa0a
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2021-10-28 00:58:21 +0200

    la til bilder, styla boksene, omtrent ferdig med produktene

--- Trines-Kantine/js/produkter.js
+++ Trines-Kantine/js/produkter.js
@@ -1,10 +1,12 @@
-const produkter = [{navn: "Rullekebab", pris: "129kr", allergener: "Gluten, laktose, egg"}, 
-{navn: "Kebab i pita", pris: "99kr", allergener: "Gluten, laktose, egg"}, 
-{navn: "Falafel", pris: "109kr", allergener: "Gluten, laktose, egg"}, 
-{navn: "Hamburger", pris: "99kr", allergener: "Gluten"}, 
-{navn: "Cheeseburger", pris: "109kr", allergener: "Gluten, laktose"}, 
-{navn: "Pizza margherita", pris: "149", allergener: "Gluten, laktose"}, 
-{navn: "Pommes frites", pris: "69kr", allergener: "Gluten"}]
+const produkter = [{navn: "Rullekebab", pris: "129kr", allergener: "Gluten, laktose, egg", src: "img/rullekebab.png"}, 
+{navn: "Kebab i pita", pris: "99kr", allergener: "Gluten, laktose, egg", src: "img/pita.png"}, 
+{navn: "Falafel", pris: "109kr", allergener: "Gluten, laktose, egg", src: "img/falafel.png"}, 
+{navn: "Hamburger", pris: "99kr", allergener: "Gluten", src: "img/hamburger.png"}, 
+{navn: "Cheeseburger", pris: "109kr", allergener: "Gluten, laktose", src: "img/cheeseburger.png"}, 
+{navn: "Pizza margherita", pris: "149kr", allergener: "Gluten, laktose", src: "img/margherita.png"}, 
+{navn: "Pommes frites", pris: "69kr", allergener: "Gluten", src: "img/pommes-frites.png"}]
+
+const filler = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
 
 function legg_Til(){
     console.log("funker");
@@ -18,6 +20,10 @@ function main(){
         div.id = produkter[i].navn
         prod_Side.appendChild(div)
 
+        let mat_bilder = document.createElement("img")
+        mat_bilder.src = produkter[i].src
+        div.appendChild(mat_bilder)
+
         let head_Navn = document.createElement("h1")
         head_Navn.innerText = produkter[i].navn
         div.appendChild(head_Navn)
@@ -27,9 +33,13 @@ function main(){
         btn.addEventListener("click", legg_Til)
         div.appendChild(btn)
 
-        let head_Pris = document.createElement("h2")
+        let head_Pris = document.createElement("h3")
         head_Pris.innerText = produkter[i].pris
         div.appendChild(head_Pris)
+
+        let produkt_informasjon = document.createElement("p")
+        produkt_informasjon.innerText = filler
+        div.appendChild(produkt_informasjon)
 
         let allergi = document.createElement("p")
         allergi.innerText = produkter[i].allergener
