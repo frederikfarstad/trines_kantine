1c9977c3c2bc1d2220fa370f5aa9c76719e6d728
Frederik Farstad <Frederf@stud.ntnu.no>
2021-11-01 21:43:26 +0100

    gjort ferdig betaling, mangler bare heading og importering av varer fra handlekurven

--- Trines-Kantine/js/betaling.js
+++ Trines-Kantine/js/betaling.js
@@ -1,6 +1,4 @@
 
-
-
 //Luhns Algoritme for kredittkortsjekk
 function luhn(){
     let card = document.getElementById("kortnummer").value;
@@ -29,5 +27,37 @@ function luhn(){
         console.log("invalid")
         return false;
     }
+
+}
+
+deliveryOptions = {
+    hjemlevering: 49,
+    hente_selv: 0,
+    baat: 99,
+    trine_personal: 999
+}
+
+//Det som ligger i handlekurven havner her når betalingssiden åpnes (DET SOM ER I LISTEN NÅ ER PLACEHOLDER)
+let shoppingCartFood = [["food1", 100], ["food2", 50]]
+
+//Regner ut prisen av matvarene når siden åpnes
+let totalFoodPrice = 0;
+for(x of shoppingCartFood){
+    totalFoodPrice += x[1];
+}
+
+//INITIALISERINGER AV VARIABLER
+let prevDelivery = deliveryOptions["hjemlevering"]; //Prisen på forrige leveringsmetode
+let total = 49 + totalFoodPrice; //49 siden siden starter checket på hjemlevering med pris 49kr
+document.getElementById("totalCost").innerText = total.toString() + ",-"; //initialiserer totalprisen på siden
+
+
+function priceCalculation(knapp){ //Regner ut prisen
+    let chosen = deliveryOptions[knapp.value];
+    total -= prevDelivery; //Tar vekk kostnaden av den forrige leveringen
+    total += chosen;
+    prevDelivery = chosen;
+    document.getElementById("deliveryStatsCost").innerText = chosen.toString() + ",-"; //Oppdaterer leveringsprisen på siden
+    document.getElementById("totalCost").innerText = total.toString() + ",-"; //Oppdaterer totalprisen på siden
 
 }
